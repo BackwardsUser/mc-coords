19e44e0e1ef509b1c52db2ed301b0c370eb4d89c
Backwards <49242785+BackwardsUser@users.noreply.github.com>
2023-07-12 14:09:48 -0400

    Converting from nether coordinates no longer requires indicating true, (from ' -n true', to -'n')

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def convert(nether, x, y, z):
     return coords
 
 parser = argparse.ArgumentParser(description="A Tool used to convert minecraft coordinates.")
-parser.add_argument('-n', '--nether', type=bool, default=False, help='Whether to convert from nether coords or overworld coords.')
+parser.add_argument('-n', '--nether', action='store_true', help='Whether to convert from nether coords or overworld coords.')
 parser.add_argument('x', type=int, default=0, help='The X Coordinate.')
 parser.add_argument('y', type=int, default=0, help='The Y Coordinate.')
 parser.add_argument('z', type=int, default=0, help='The Z Coordinate.')
@@ -22,4 +22,4 @@ args = parser.parse_args();
 
 conversion = convert(args.nether, args.x, args.y, args.z)
 
-print(f'From X:{args.x}, Y:{args.y}, Z:{args.z} to X:{conversion[0]}, Y:{conversion[1]}, Z:{conversion[2]}.')
+print(f'Given Coordinates X:{args.x}, Y:{args.y}, Z:{args.z}\nConverted Coordinates: X:{conversion[0]}, Y:{conversion[1]}, Z:{conversion[2]}.')
